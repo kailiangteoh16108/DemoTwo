344728d7dd7d6aec65c774a9d48243b98e710aff
TEOH KAI LIANG <symparkmanagement915@gmail.com>
2022-11-21 01:25:56 +0800

    First commit of the project

---
+++ README.md
@@ -0,0 +1,2 @@
+This is the readme file. 
+In this demo two, we will just print lines. 
---
+++ main.py
@@ -0,0 +1,3 @@
+print("Hello, my name is Teoh Kai Liang")
+print("This is the second project that I coded in PyCharm.")
+print("Git is useful to collborate with other people in programming.")
